94f99a53c9eaf0654698571c227b57fbe7b28601
Andres Ruiz Acevedo <andresruizaceved@gmail.com>
2023-05-16 11:52:30 +1000

    Fixing Enum and code duplication

--- README.md
+++ README.md
@@ -3,3 +3,94 @@
 This repository contains the code for the Python code smells video on the ArjanCodes channel (watch the video [here](https://youtu.be/LrtnLEkOwFE)).
 
 The example is about employees at a company. In `before.py`, you find the original code (containing 7 code smells + a bonus smell). The `after.py` file contains the same code, but a lot less smelly.
+
+## Using strings instead of `Enum` for defined categories
+
+```
+from enum import Enum, auto   # <-- Import these
+
+class Role(Enum):             # <-- Define a class with the allowed values
+    """Employee roles."""
+
+    PRESIDENT = auto()
+    VICEPRESIDENT = auto()
+    MANAGER = auto()
+    LEAD = auto()
+    WORKER = auto()
+    INTERN = auto()
+
+@dataclass
+class Employee:
+    """Basic representation of an employee at the company."""
+
+    name: str
+    role: Role               # <-- Replace 'str' with Enum class
+
+
+class Company:
+    """Represents a company with employees."""
+
+    def __init__(self) -> None:
+        self.employees: List[Employee] = []
+
+    def add_employee(self, employee: Employee) -> None:
+        """Add an employee to the list of employees."""
+        self.employees.append(employee)
+
+```
+
+```
+def main() -> None:
+    """Main function."""
+
+    company = Company()
+    company.add_employee(
+        SalariedEmployee(
+            name="Louis", 
+            role=Role.MANAGER   # <-- Use instead of 'str'
+        )
+    )
+```
+
+### Code duplication
+
+```
+# Within the scope of 'class Company()`
+
+    def find_managers(self) -> List[Employee]:
+        """Find all manager employees."""
+        managers = []
+        for employee in self.employees:
+            if employee.role == Role.MANAGER:
+                managers.append(employee)
+        return managers
+
+    def find_vice_presidents(self) -> List[Employee]:
+        """Find all vice-president employees."""
+        vice_presidents = []
+        for employee in self.employees:
+            if employee.role == Role.VICEPRESIDENT:
+                vice_presidents.append(employee)
+        return vice_presidents
+
+    def find_interns(self) -> List[Employee]:
+        """Find all interns."""
+        interns = []
+        for employee in self.employees:
+            if employee.role == Role.INTERN:
+                interns.append(employee)
+        return interns
+```
+
+Do this instead:
+```
+    def find_employees(self, role: Role) -> List[Employee]:
+        """Find all employees with a particular role."""
+        employees = []
+        for employee in self.employees:
+            if employee.role == role:
+                employees.append(employee)
+        return employees
+
+```
+
--- before-ara.py
+++ before-ara.py
@@ -3,9 +3,21 @@ Very advanced Employee management system.
 """
 
 from dataclasses import dataclass
+from enum import Enum, auto
 from typing import List
 
 FIXED_VACATION_DAYS_PAYOUT = 5  # The fixed nr of vacation days that can be paid out.
+
+
+class Role(Enum):
+    """Employee roles."""
+
+    PRESIDENT = auto()
+    VICEPRESIDENT = auto()
+    MANAGER = auto()
+    LEAD = auto()
+    WORKER = auto()
+    INTERN = auto()
 
 
 @dataclass
@@ -13,7 +25,7 @@ class Employee:
     """Basic representation of an employee at the company."""
 
     name: str
-    role: str
+    role: Role
     vacation_days: int = 25
 
     def take_a_holiday(self, payout: bool) -> None:
@@ -65,29 +77,13 @@ class Company:
         """Add an employee to the list of employees."""
         self.employees.append(employee)
 
-    def find_managers(self) -> List[Employee]:
-        """Find all manager employees."""
-        managers = []
+    def find_employees(self, role: Role) -> List[Employee]:
+        """Find all employees with a particular role."""
+        employees = []
         for employee in self.employees:
-            if employee.role == "manager":
-                managers.append(employee)
-        return managers
-
-    def find_vice_presidents(self) -> List[Employee]:
-        """Find all vice-president employees."""
-        vice_presidents = []
-        for employee in self.employees:
-            if employee.role == "vice_president":
-                vice_presidents.append(employee)
-        return vice_presidents
-
-    def find_interns(self) -> List[Employee]:
-        """Find all interns."""
-        interns = []
-        for employee in self.employees:
-            if employee.role == "intern":
-                interns.append(employee)
-        return interns
+            if employee.role == role:
+                employees.append(employee)
+        return employees
 
     def pay_employee(self, employee: Employee) -> None:
         """Pay an employee."""
@@ -107,13 +103,13 @@ def main() -> None:
 
     company = Company()
 
-    company.add_employee(SalariedEmployee(name="Louis", role="manager"))
-    company.add_employee(HourlyEmployee(name="Brenda", role="president"))
-    company.add_employee(HourlyEmployee(name="Tim", role="intern"))
+    company.add_employee(SalariedEmployee(name="Louis", role=Role.MANAGER))
+    company.add_employee(HourlyEmployee(name="Brenda", role=Role.PRESIDENT))
+    company.add_employee(HourlyEmployee(name="Tim", role=Role.INTERN))
 
-    print(company.find_vice_presidents())
-    print(company.find_managers())
-    print(company.find_interns())
+    print(company.find_employees(role=Role.VICEPRESIDENT))
+    print(company.find_employees(role=Role.MANAGER))
+    print(company.find_employees(role=Role.INTERN))
     company.pay_employee(company.employees[0])
     company.employees[0].take_a_holiday(False)
 
